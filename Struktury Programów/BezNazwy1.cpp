51c388508af58b267cc3d6dd4c3fd9e26ebeae2c
GwiazdaHollywood <150920670+GwiazdaHollywood@users.noreply.github.com>
2023-11-26 16:04:55 +0100

    Add files via upload

--- Struktury Programów/BezNazwy1.cpp
+++ Struktury Programów/BezNazwy1.cpp
@@ -1,19 +1,30 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-	// while(warunek-zakonczenia(true))
-	short a = 2;
-	while(a <= 30){        // czy wykonac to...?
-		cout << a << ", ";
-		a+= 2;
-	}
-	cout << endl << endl;
-	// do... while()
-	
-	short liczba;
-	do{
-		cout << "Podaj liczbe: ";
-		cin >> liczba;
-	}while(liczba != 5);     // czy powtorzyc to...?
+int main() {
+	    // Zmiana count na 0 aby œledziæ liczby bez powtórzeñ
+    int count = 0;
+
+    // Pêtla dla pierwszej cyfry (setki)
+    for (int i = 1; i <= 9; ++i) {
+    	        // Pêtla dla drugiej cyfry (dziesi¹tki)
+        for (int j = 0; j <= 9; ++j) {
+        	            // Pêtla dla trzeciej cyfry (jednoœci)
+            for (int k = 0; k <= 9; ++k) {
+            	                // Sprawdzamy czy cyfry sie roznia od siebie
+                if (i != j && j != k && i != k) {
+                	
+                 //Wypisanie trzycyfrowych liczb i wyswietlanie
+
+                    int number = i * 100 + j * 10 + k;
+                    cout << number << endl;
+                    count++;
+                }
+            }
+        }
+    }
+ // tu wyswietli liczby bez powtorzen gdy powyzsza petla sie zakonczy
+    cout << "Liczba liczb bez powtarzajacych sie cyfr: " << count << endl;
+
+    return 0;
 }
